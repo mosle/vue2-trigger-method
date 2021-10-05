e341b20c60460978dfc89d595b85345bccd1d9d9
mosle <>
2021-10-05 09:16:45 +0900

    first import

---
+++ dev/serve.vue
@@ -0,0 +1,36 @@
+<script lang="ts">
+import Vue from "vue";
+import TriggerMethod from "@/trigger-method.vue";
+
+export default Vue.extend({
+  name: "ServeDev",
+  data() {
+    return {
+      a: "scope test A",
+      b: "scope test B",
+    };
+  },
+  components: {
+    TriggerMethod,
+  },
+  methods: {
+    test1() {
+      console.log(`test 1 called (${this.a})`);
+    },
+    test2() {
+      console.log(`test 2 called (${this.b})`);
+    },
+    test3(arg: string) {
+      console.log(`test 3 called ${arg}`);
+    },
+  },
+});
+</script>
+
+<template>
+  <div id="app">
+    <trigger-method @trigger="test1" />
+    <trigger-method :delay="1000" @trigger="test2" />
+    <trigger-method :delay="2000" @trigger="test3('pass argument')" />
+  </div>
+</template>
---
+++ src/trigger-method.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import Vue from "vue";
+
+interface DataType {}
+
+export default /*#__PURE__*/ Vue.extend({
+  name: "TriggerMethod", // vue component name
+  props: {
+    delay: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data(): DataType {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  created(): void {
+    setTimeout(() => {
+      this.$emit("trigger");
+    }, this.delay);
+  },
+});
+</script>
+
+<template></template>
+
+<style scoped>
+</style>
